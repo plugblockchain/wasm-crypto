polkadot-js/wasm

Add asm.js generation with CRYPTO_ASM env var (#23)
* Add asm.js generation, import via index-asm.js

* Less sed-y, more babel-y

* Throw error when some failed

* CRYPTO_ASM env flag

* CHANGELOG

packages/wasm-crypto/src/js/exports.js
@@ -1,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Copyright 2019 @polkadot/wasm-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+const { assert } = require('@polkadot/util');
+
+const INIT_ERRROR = '@polkadot/wasm-crypto has not been initialized';
+
+module.exports = function (stubbed) {
+  const wrapReady = (fn) =>
+    (...params) => {
+      assert(stubbed.isReady(), INIT_ERRROR);
+
+      return fn(...params);
+    };
+
+  return {
+    bip39Generate: wrapReady(stubbed.ext_bip39_generate),
+    bip39ToEntropy: wrapReady(stubbed.ext_bip39_to_entropy),
+    bip39ToMiniSecret: wrapReady(stubbed.ext_bip39_to_mini_secret),
+    bip39ToSeed: wrapReady(stubbed.ext_bip39_to_seed),
+    bip39Validate: wrapReady(stubbed.ext_bip39_validate),
+
+    ed25519KeypairFromSeed: wrapReady(stubbed.ext_ed_from_seed),
+    ed25519Sign: wrapReady(stubbed.ext_ed_sign),
+    ed25519Verify: wrapReady(stubbed.ext_ed_verify),
+
+    sr25519DeriveKeypairHard: wrapReady(stubbed.ext_sr_derive_keypair_hard),
+    sr25519DeriveKeypairSoft: wrapReady(stubbed.ext_sr_derive_keypair_soft),
+    sr25519DerivePublicSoft: wrapReady(stubbed.ext_sr_derive_public_soft),
+    sr25519KeypairFromSeed: wrapReady(stubbed.ext_sr_from_seed),
+    sr25519Sign: wrapReady(stubbed.ext_sr_sign),
+    sr25519Verify: wrapReady(stubbed.ext_sr_verify),
+
+    blake2b: wrapReady(stubbed.ext_blake2b),
+    keccak256: wrapReady(stubbed.ext_keccak256),
+    pbkdf2: wrapReady(stubbed.ext_pbkdf2),
+
+    // secp256k1IsRecoverable: wrapReady(stubbed.ext_secp256k1_is_recoverable);
+    // secp256k1Recover: wrapReady(stubbed.ext_secp256k1_recover);
+
+    sha512: wrapReady(stubbed.ext_sha512),
+    twox: wrapReady(stubbed.ext_twox),
+
+    isReady: stubbed.isReady,
+    waitReady: stubbed.waitReady
+  };
+};

scripts/build-package.sh
@@ -16,11 +16,13 @@ mv pkg build
 # shortcuts for files
 echo "*** Adjusting output"
 BGJ=build/wasm_bg.js
-SRC=build/wasm.js
+SRC_ASM=build/asm.js
+SRC_WASM=build/wasm.js
 DEF=build/wasm.d.ts
 WSM=build/wasm_bg.wasm
 OPT=build/wasm_opt.wasm
 ASM=build/wasm_asm.js
+TMP=build/wasm_tmp.js
 
 # optimise
 echo "*** Optimising WASM output"
@@ -29,7 +31,15 @@ echo "*** Optimising WASM output"
 
 # build asmjs version from the input (optimised) WASM
 # echo "*** Building asm.js version"
-# ../../binaryen/bin/wasm2js --no-validation --output $ASM $OPT
+echo "*** Building asm.js version"
+../../binaryen/bin/wasm2js --no-validation --output $TMP $OPT
+
+# cleanup asm
+sed -i -e 's/\.\/wasm/\.\/asm/g' $TMP
+
+# get babel to take care of making it sane
+NODE_OPTIONS=--max_old_space_size=8192 yarn babel $TMP --presets @babel/preset-env --out-file=$ASM
+rm -rf $TMP
 
 # convert wasm to base64 structure
 echo "*** Packing WASM into base64"
@@ -39,16 +49,26 @@ node ../../scripts/pack-wasm-base64.js
 echo "*** Copying package sources"
 cp src/js/* build/
 
-# cleanup asm
-# sed -i -e 's/import {/\/\/ import {/g' $ASM
-# sed -i -e 's/function asmFunc/var schnorrkel = require('\''\.\/wasm'\''); function asmFunc/g' $ASM
-# sed -i -e 's/export const /module\.exports\./g' $ASM
-# sed -i -e 's/{abort.*},memasmFunc/schnorrkel, memasmFunc/g' $ASM
-
 # we are swapping to a async interface for webpack support (wasm limits)
-sed -i -e 's/wasm = require/\/\/ wasm = require/g' $SRC
+sed -i -e 's/wasm = require/\/\/ wasm = require/g' $SRC_WASM
 
-# construct our promise and add ready helpers
+# We don't want inline requires
+sed -i -e 's/var wasm;/const crypto = require('\''crypto'\''); let wasm; const requires = { crypto };/g' $SRC_WASM
+sed -i -e 's/return addHeapObject(require(varg0));/return addHeapObject(requires[varg0]);/g' $SRC_WASM
+
+# this creates issues in both the browser and RN (@polkadot/util has a polyfill)
+sed -i -e 's/const TextEncoder = require('\''util'\'')\.TextEncoder;/const { stringToU8a } = require('\''@polkadot\/util'\'');/g' $SRC_WASM
+sed -i -e 's/let cachedTextEncoder = new /\/\/ let cachedTextEncoder = new /g' $SRC_WASM
+sed -i -e 's/cachedTextEncoder\.encode/stringToU8a/g' $SRC_WASM
+
+# this creates issues in both the browser and RN (@polkadot/util has a polyfill)
+sed -i -e 's/const TextDecoder = require('\''util'\'')\.TextDecoder;/const { u8aToString } = require('\''@polkadot\/util'\'');/g' $SRC_WASM
+sed -i -e 's/let cachedTextDecoder = new /\/\/ let cachedTextDecoder = new /g' $SRC_WASM
+sed -i -e 's/cachedTextDecoder\.decode/u8aToString/g' $SRC_WASM
+
+cp -f $SRC_WASM $SRC_ASM
+
+# construct our promise and add ready helpers (WASM)
 echo "
 module.exports.abort = function () { throw new Error('abort'); };
 
@@ -59,7 +79,20 @@ module.exports.isReady = function () { return !!wasm; }
 module.exports.waitReady = function () { return wasmPromise.then(() => !!wasm); }
 
 wasmPromise.then((_wasm) => { wasm = _wasm });
-" >> $SRC
+" >> $SRC_WASM
+
+# construct our promise and add ready helpers (ASM)
+echo "
+module.exports.abort = function () { throw new Error('abort'); };
+
+const asmjs = require('./wasm_asm');
+const wasmPromise = Promise.resolve(asmjs);
+
+module.exports.isReady = function () { return !!wasm; }
+module.exports.waitReady = function () { return wasmPromise.then(() => !!wasm); }
+
+wasmPromise.then((_wasm) => { wasm = _wasm });
+" >> $SRC_ASM
 
 # add extra methods to type definitions
 echo "
@@ -79,9 +112,12 @@ module.exports = async function createExportPromise () {
   };
 
   if (!WebAssembly) {
-    console.error(\`ERROR: Unable to initialize \${pkg.name}, WebAssembly is not available in this environment\`);
+    if (process.env.CRYPTO_ASM) {
+      return require('./wasm_asm');
+    }
 
-    // TODO: Return asm version when not detected
+    console.error(\`ERROR: Unable to initialize \${pkg.name}, WebAssembly is not available in this environment. If you requires an asm.js fallback, specify it with the 'CRYPTO_ASM' environment flag.\`);
+
     return null;
   }
 

plugblockchain/wasm-crypto

packages/wasm-crypto/src/js/exports.js
@@ -1,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Copyright 2019 @polkadot/wasm-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+const { assert } = require('@plugnet/util');
+
+const INIT_ERRROR = '@plugnet/wasm-crypto has not been initialized';
+
+module.exports = function (stubbed) {
+  const wrapReady = (fn) =>
+    (...params) => {
+      assert(stubbed.isReady(), INIT_ERRROR);
+
+      return fn(...params);
+    };
+
+  return {
+    bip39Generate: wrapReady(stubbed.ext_bip39_generate),
+    bip39ToEntropy: wrapReady(stubbed.ext_bip39_to_entropy),
+    bip39ToMiniSecret: wrapReady(stubbed.ext_bip39_to_mini_secret),
+    bip39ToSeed: wrapReady(stubbed.ext_bip39_to_seed),
+    bip39Validate: wrapReady(stubbed.ext_bip39_validate),
+
+    ed25519KeypairFromSeed: wrapReady(stubbed.ext_ed_from_seed),
+    ed25519Sign: wrapReady(stubbed.ext_ed_sign),
+    ed25519Verify: wrapReady(stubbed.ext_ed_verify),
+
+    sr25519DeriveKeypairHard: wrapReady(stubbed.ext_sr_derive_keypair_hard),
+    sr25519DeriveKeypairSoft: wrapReady(stubbed.ext_sr_derive_keypair_soft),
+    sr25519DerivePublicSoft: wrapReady(stubbed.ext_sr_derive_public_soft),
+    sr25519KeypairFromSeed: wrapReady(stubbed.ext_sr_from_seed),
+    sr25519Sign: wrapReady(stubbed.ext_sr_sign),
+    sr25519Verify: wrapReady(stubbed.ext_sr_verify),
+
+    blake2b: wrapReady(stubbed.ext_blake2b),
+    keccak256: wrapReady(stubbed.ext_keccak256),
+    pbkdf2: wrapReady(stubbed.ext_pbkdf2),
+
+    // secp256k1IsRecoverable: wrapReady(stubbed.ext_secp256k1_is_recoverable);
+    // secp256k1Recover: wrapReady(stubbed.ext_secp256k1_recover);
+
+    sha512: wrapReady(stubbed.ext_sha512),
+    twox: wrapReady(stubbed.ext_twox),
+
+    isReady: stubbed.isReady,
+    waitReady: stubbed.waitReady
+  };
+};

scripts/build-package.sh
@@ -16,11 +16,13 @@ mv pkg build
 # shortcuts for files
 echo "*** Adjusting output"
 BGJ=build/wasm_bg.js
-SRC=build/wasm.js
+SRC_ASM=build/asm.js
+SRC_WASM=build/wasm.js
 DEF=build/wasm.d.ts
 WSM=build/wasm_bg.wasm
 OPT=build/wasm_opt.wasm
 ASM=build/wasm_asm.js
+TMP=build/wasm_tmp.js
 
 # optimise
 echo "*** Optimising WASM output"
@@ -29,7 +31,15 @@ echo "*** Optimising WASM output"
 
 # build asmjs version from the input (optimised) WASM
 # echo "*** Building asm.js version"
-# ../../binaryen/bin/wasm2js --no-validation --output $ASM $OPT
+echo "*** Building asm.js version"
+../../binaryen/bin/wasm2js --no-validation --output $TMP $OPT
+
+# cleanup asm
+sed -i -e 's/\.\/wasm/\.\/asm/g' $TMP
+
+# get babel to take care of making it sane
+NODE_OPTIONS=--max_old_space_size=8192 yarn babel $TMP --presets @babel/preset-env --out-file=$ASM
+rm -rf $TMP
 
 # convert wasm to base64 structure
 echo "*** Packing WASM into base64"
@@ -39,16 +49,26 @@ node ../../scripts/pack-wasm-base64.js
 echo "*** Copying package sources"
 cp src/js/* build/
 
-# cleanup asm
-# sed -i -e 's/import {/\/\/ import {/g' $ASM
-# sed -i -e 's/function asmFunc/var schnorrkel = require('\''\.\/wasm'\''); function asmFunc/g' $ASM
-# sed -i -e 's/export const /module\.exports\./g' $ASM
-# sed -i -e 's/{abort.*},memasmFunc/schnorrkel, memasmFunc/g' $ASM
-
 # we are swapping to a async interface for webpack support (wasm limits)
-sed -i -e 's/wasm = require/\/\/ wasm = require/g' $SRC
+sed -i -e 's/wasm = require/\/\/ wasm = require/g' $SRC_WASM
 
-# construct our promise and add ready helpers
+# We don't want inline requires
+sed -i -e 's/var wasm;/const crypto = require('\''crypto'\''); let wasm; const requires = { crypto };/g' $SRC_WASM
+sed -i -e 's/return addHeapObject(require(varg0));/return addHeapObject(requires[varg0]);/g' $SRC_WASM
+
+# this creates issues in both the browser and RN (@plugnet/util has a polyfill)
+sed -i -e 's/const TextEncoder = require('\''util'\'')\.TextEncoder;/const { stringToU8a } = require('\''@plugnet\/util'\'');/g' $SRC_WASM
+sed -i -e 's/let cachedTextEncoder = new /\/\/ let cachedTextEncoder = new /g' $SRC_WASM
+sed -i -e 's/cachedTextEncoder\.encode/stringToU8a/g' $SRC_WASM
+
+# this creates issues in both the browser and RN (@plugnet/util has a polyfill)
+sed -i -e 's/const TextDecoder = require('\''util'\'')\.TextDecoder;/const { u8aToString } = require('\''@plugnet\/util'\'');/g' $SRC_WASM
+sed -i -e 's/let cachedTextDecoder = new /\/\/ let cachedTextDecoder = new /g' $SRC_WASM
+sed -i -e 's/cachedTextDecoder\.decode/u8aToString/g' $SRC_WASM
+
+cp -f $SRC_WASM $SRC_ASM
+
+# construct our promise and add ready helpers (WASM)
 echo "
 module.exports.abort = function () { throw new Error('abort'); };
 
@@ -59,7 +79,20 @@ module.exports.isReady = function () { return !!wasm; }
 module.exports.waitReady = function () { return wasmPromise.then(() => !!wasm); }
 
 wasmPromise.then((_wasm) => { wasm = _wasm });
-" >> $SRC
+" >> $SRC_WASM
+
+# construct our promise and add ready helpers (ASM)
+echo "
+module.exports.abort = function () { throw new Error('abort'); };
+
+const asmjs = require('./wasm_asm');
+const wasmPromise = Promise.resolve(asmjs);
+
+module.exports.isReady = function () { return !!wasm; }
+module.exports.waitReady = function () { return wasmPromise.then(() => !!wasm); }
+
+wasmPromise.then((_wasm) => { wasm = _wasm });
+" >> $SRC_ASM
 
 # add extra methods to type definitions
 echo "
@@ -79,9 +112,12 @@ module.exports = async function createExportPromise () {
   };
 
   if (!WebAssembly) {
-    console.error(\`ERROR: Unable to initialize \${pkg.name}, WebAssembly is not available in this environment\`);
+    if (process.env.CRYPTO_ASM) {
+      return require('./wasm_asm');
+    }
 
-    // TODO: Return asm version when not detected
+    console.error(\`ERROR: Unable to initialize \${pkg.name}, WebAssembly is not available in this environment. If you requires an asm.js fallback, specify it with the 'CRYPTO_ASM' environment flag.\`);
+
     return null;
   }
 