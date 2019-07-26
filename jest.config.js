polkadot-js/wasm

Swap to eslint (#22)
* Swap to eslint

* Remove links to outdated packages

* Bump hex_literal to 0.2.0

* Bump version

jest.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('@polkadot/dev/config/jest');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
-    '@polkadot/wasm-(schnorrkel)(.*)$': '<rootDir>/packages/wasm-$1/src/$2',
+    '@polkadot/wasm-(schnorrkel)(.*)$': '<rootDir>/packages/wasm-$1/src/$2'
   },
   modulePathIgnorePatterns: [
     '<rootDir>/packages/wasm-schnorrkel/build'

plugblockchain/wasm-crypto

jest.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('@plugnet/dev/config/jest');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
-    '@plugnet/wasm-(schnorrkel)(.*)$': '<rootDir>/packages/wasm-$1/src/$2',
+    '@plugnet/wasm-(schnorrkel)(.*)$': '<rootDir>/packages/wasm-$1/src/$2'
   },
   modulePathIgnorePatterns: [
     '<rootDir>/packages/wasm-schnorrkel/build'