9dbcbf170d937cd5e3728390c48949446d2275e4
Jaco Greeff <jacogr@gmail.com>
2019-08-30 12:11:20 +0200

    typescript-eslint 2 (#33)
    * typescript-eslint 2
    
    * Blind eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = require('@plugnet/dev/config/eslint');
+const base = require('@plugnet/dev/config/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    extraFileExtensions: ['*.d.ts'],
+    project: [
+      './tsconfig.eslint.json'
+    ]
+  }
+};
