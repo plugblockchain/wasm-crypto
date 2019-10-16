polkadot-js/wasm

typescript-eslint 2 (#33)
* typescript-eslint 2

* Blind eslint fixes

.eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = require('@polkadot/dev/config/eslint');
+const base = require('@polkadot/dev/config/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    extraFileExtensions: ['*.d.ts'],
+    project: [
+      './tsconfig.eslint.json'
+    ]
+  }
+};

plugblockchain/wasm-crypto

.eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = require('@plugnet/dev/config/eslint');
+const base = require('@plugnet/dev/config/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    extraFileExtensions: ['*.d.ts'],
+    project: [
+      './tsconfig.eslint.json'
+    ]
+  }
+};